be7daa6a70b0c829af114558458ccb057bc1d34c
Brian Bauska <brianbauska@gmail.com>
2025-02-04 12:57:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const url =
 	'https://api.openweathermap.org/data/2.5/weather';
 const apiKey =
 	'f00c38e0279b7bc85480c3fe775d518c';
+var farenheitTemp = 0;
 
 $(document).ready(function () {
-	weatherFn('Seattle');
+	weatherFn('Sultan');
 });
 
 async function weatherFn(cName) {
@@ -14,6 +15,7 @@ async function weatherFn(cName) {
 		const res = await fetch(temp);
 		const data = await res.json();
 		if (res.ok) {
+			farenheitTemp = 32 + ((data.main.temp * 9)/5)
 			weatherShowFn(data);
 		} else {
 			alert('City not found. Please try again.');
